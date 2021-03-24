78a9fd1c62a8355acb6aa7d97735dc4085f2be3a
bdjm <brendandjmoore@gmail.com>
2021-03-24 23:53:47 +1000

    moving the quiz questions to the quiz questions script

---
+++ assets/javascript/quizquestions.js
@@ -0,0 +1,89 @@
+var questions = [
+    {
+      title: "In the JavaScript, which one of the following is not considered as an error?",
+      choices: ["Syntax error", "Missing of semicolons", "Division by zero", "Missing of Bracket"],
+      answer: "Division by zero"
+    },
+    {
+      title: "Which of the following number object function returns the value of the number?",
+      choices: [
+        "toString()",
+        "valueOf()",
+        "toLocaleString()",
+        "toPrecision()"
+      ],
+      answer: "valueOf()"
+    },
+    {
+      title: "Which of the following element is responsible for making the text bold in HTML",
+      choices: ["<pre>", "<a>", "<b>", "<br>"],
+      answer: "<b>"
+    },
+    {
+      title: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All of the above"
+      ],
+      answer: "All of the above"
+    },
+    {
+      title:
+        "What are the types of unordered or bulleted list in HTML?",
+      choices: [
+        "disc, square, triangle",
+        "polygon, triangle, circle",
+        "disc, circle, square",
+        "All of the above"
+      ],
+      answer: "disc, circle, square"
+    },
+    {
+      title: "Which of the following is correct about Bootstrap cross browser consistency?",
+      choices: [
+        "Bootstrap uses Normalize to establish cross browser consistency",
+        "Normalize.css is a modern, HTML5-ready alternative to CSS resets",
+        "Normalize.css is a small CSS file that provides better cross-browser consistency in the default styling of HTML elements",
+        "All of the above"
+      ],
+      answer: "All of the above"
+    },
+    {
+      title: "Which of the following is correct about Bootstrap jumbotron?",
+      choices: [
+        "This component can optionally increase the size of headings and add a lot of margin for landing page content", 
+      "To use the Jumbotron: Create a container <div> with the class of .jumbotron", 
+      "Both of the above", 
+      "None of the above"],
+      answer: "Both of the above"
+    },
+    {
+      title: "Which of the following sign is used as a shortcut for jQuery?",
+      choices: [
+        "the % sign",
+        "the & sign",
+        "the $ sign",
+        "the @ sign"
+      ],
+      answer: "the $ sign"
+    },
+    {
+      title:
+        "Which of the following CSS property is used to set the horizontal alignment of a table-cell box or the block element",
+      choices: ["text-align property", "text-transform property", "text-shadow property", "text-decoration"],
+      answer: "text-align property"
+    },
+    {
+      title:
+        "Which of the following jQuery method is used to stop jQuery for few milliseconds?",
+      choices: ["stop() method", "delay() method", "slowdown() method", "pause() method"],
+      answer: "delay() method"
+    },
+    {
+      title: "If the names of variables are the same, then which of the following type of variable takes precedence?",
+      choices: ["local variable", "global variable", "both of local and global variable", "None of the above"],
+      answer: "local variable"
+    }
+  ];
