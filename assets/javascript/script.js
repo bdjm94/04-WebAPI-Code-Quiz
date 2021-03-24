b79ae2911ce20043a4eeae9210d32753570a1e8c
bdjm <brendandjmoore@gmail.com>
2021-03-24 22:51:02 +1000

    adding script for the questions of the quiz

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,8 +21,47 @@ fuction startQuiz() {
 
 function getQuestion() {
     var currentQuestion = questions[currentQuestionNumber];
-}
+
 
 var titleEl = document.getElementById("question-title");
 titleEl.textContent = currentQuestion.title;
 choicesEl.innerHTML = "";
+
+currentQuestion.choices.forEach(function(choice, i) {
+    var choiceButton = document.createElement("button");
+    choiceButton.setAttribute("class", "choice");
+    choiceButton.setAttribute("value", choice);
+    choiceButton.textContent = i + 1 + ". " + choice;
+    choiceButton.onClick = questionClick;
+    choicesEl.appendChild(choiceButton);
+});
+}
+
+function questionClick() {
+    if (this.value !== questions[currentQuestionNumber].answer) {
+        time -= 10;
+
+        if(time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        feedbackEl.textContent = "Incorrect";
+        feedbackEl.style.color = "red";
+        feedbackEl.style.fontSize = "200%"
+    } else {
+        feedbackEl.textContent = "Correct";
+        feedbackEl.style.color = "green";
+        feedbackEl.style.fontSize = "200%";
+    }
+    
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+    currentQuestionNumber++;
+    if(currentQuestionNumber === questions.length) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
