dd129feb146f4bff112535cb2da9e2563e6d0bae
bdjm <brendandjmoore@gmail.com>
2021-03-24 22:24:09 +1000

    adding script to hide and unide start screen/questions section

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,13 @@ var submitBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
 var initialsEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
+
+var currentQuestionNumber = 0;
+var time = questions.length * 15;
+var timerId;
+
+fuction quizStart() {
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+}
