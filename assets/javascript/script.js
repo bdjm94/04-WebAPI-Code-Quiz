105f15351e4aeeaed346642d774f57d17318e3e4
bdjm <brendandjmoore@gmail.com>
2021-03-24 23:57:35 +1000

    adding a link to the highscores html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -98,6 +98,7 @@ function saveHighscore() {
         };
     highscores.push(newScore);
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    window.location.href = "highscores.html";
     }
 }
 
