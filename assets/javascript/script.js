a15b76e88120b82d5182c08c4f2fd6645d171229
bdjm <brendandjmoore@gmail.com>
2021-03-24 22:34:26 +1000

    adding function to get questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,8 +10,19 @@ var currentQuestionNumber = 0;
 var time = questions.length * 15;
 var timerId;
 
-fuction quizStart() {
+fuction startQuiz() {
     var startScreenEl = document.getElementById("start-screen");
     startScreenEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
+    timerId = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    getQuestion();
 }
+
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionNumber];
+}
+
+var titleEl = document.getElementById("question-title");
+titleEl.textContent = currentQuestion.title;
+choicesEl.innerHTML = "";
